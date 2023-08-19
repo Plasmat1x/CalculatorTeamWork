25382a443e1195df3770f2f5c307fed5dc0a3eec
Mikhail Mryasov <damaghack@gmail.com>
2023-08-19 17:20:07 +0700

    Add test version

--- TWCalculator/Views/MainWindow.xaml.cs
+++ TWCalculator/Views/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CalcTW;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TWCalculator.Views
 {
@@ -19,9 +8,65 @@ namespace TWCalculator.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        float LOperand;
+        float ROperand;
+        bool Point;
+
+        public delegate Result Operation(float l, float r);
+
+        Operation operation;
+
+        Calculator calculator;
+
         public MainWindow()
         {
             InitializeComponent();
+            calculator = new Calculator();
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(inputField.Text))
+            {
+                float.TryParse(inputField.Text, out LOperand);
+                inputField.Text = "";
+                operation = calculator.Add;
+            }
+
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            inputField.Text += "2";
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            inputField.Text += "1";
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+            inputField.Text += "3";
+        }
+
+        private void btRes_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(inputField.Text))
+            {
+                float.TryParse(inputField.Text, out ROperand);
+
+                Result res = operation.Invoke(LOperand, ROperand);
+
+                if (res.status == Status.SUCCESS)
+                {
+                    inputField.Text = res.Value.ToString();
+                }
+                else
+                {
+                    inputField.Text = "ERROR";
+                }
+            }
         }
     }
 }
