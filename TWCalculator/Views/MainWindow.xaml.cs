4340a25d0c382cbac1b346e32d515d6733097f68
Mikhail Mryasov <damaghack@gmail.com>
2023-08-22 21:47:57 +0700

    Update Form

--- TWCalculator/Views/MainWindow.xaml.cs
+++ TWCalculator/Views/MainWindow.xaml.cs
@@ -9,21 +9,21 @@ namespace TWCalculator.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        float LOperand;
-        float ROperand;
+        private float LOperand;
+        private float ROperand;
 
-        bool Res;
+        private bool Res;
 
-        delegate Result Operation(float l, float r);
+        private delegate Result Operation(float l, float r);
 
-        Operation operation;
-
-        Calculator calculator;
+        private Operation? operation;
+        private Calculator calculator;
 
         public MainWindow()
         {
             InitializeComponent();
             calculator = new Calculator();
+            operation = null;
         }
 
         private void bt1_Click(object sender, RoutedEventArgs e)
